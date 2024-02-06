3215152a65ac39a9028ee57506151f28df526435
goodtimeswithpaul <moi@paultissot.ch>
2024-02-06 17:22:31 +0100

    feat: add a GitHandler class (#6)

---
+++ src/main/java/org/example/GitHandler.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import static org.example.Main.logger;
+
+public class GitHandler {
+
+    public GitHandler(String repoPath) {
+        logger.info("Git handler was created");
+    }
+
+}
